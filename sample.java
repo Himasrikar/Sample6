1fa750b6097b5ecf7c4c0e615ee36d14536da252
Himasrikar <himasrikar00@gmail.com>
2024-12-08 20:10:12 +0530

    User1 commited code

---
+++ sample.java
@@ -0,0 +1,24 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers between 1 and 100 are:");
+        for (int num = 2; num <= 100; num++) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+simple la 6 question di
